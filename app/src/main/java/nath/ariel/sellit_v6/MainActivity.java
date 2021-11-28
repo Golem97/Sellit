dcb6d1d0c61f7e094974f2d4b142d0abe9804674
NathBen97 <74254759+Golem97@users.noreply.github.com>
2021-11-28 16:35:37 +0200

    first

---
+++ app/src/main/java/nath/ariel/sellit_v6/MainActivity.java
@@ -0,0 +1,14 @@
+package nath.ariel.sellit_v6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
