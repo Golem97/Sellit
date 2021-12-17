534d1bc4a36d0bab1ce8ac98736a8de005f33aec
Jewgah <jordanlloydperez@gmail.com>
2021-12-17 13:39:48 +0200

    SellerActivity image upload

--- app/src/main/java/classes/Item.java
+++ app/src/main/java/classes/Item.java
@@ -1,17 +1,73 @@
 package classes;
 
+import android.net.Uri;
+
 /**
  * Created by Jordan Perez on 15/12/2021
  */
 public class Item {
 
-    private int itemId;
+    private String user_id;
     private String name;
     private String description;
     private int price;
     private boolean available;
-//    private boolean inRequest;
+    private String photoUrl;
 
+    public Item(String user_id, String name, String description, int price, boolean available, String photoUrl) {
+        this.user_id = user_id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.available = available;
+        this.photoUrl = photoUrl;
+    }
 
+    public String getUser_id() {
+        return user_id;
+    }
 
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
 }
--- app/src/main/java/nath/ariel/sellit_v6/activities/SellerActivity.java
+++ app/src/main/java/nath/ariel/sellit_v6/activities/SellerActivity.java
@@ -1,9 +1,18 @@
 package nath.ariel.sellit_v6.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import classes.Item;
 import nath.ariel.sellit_v6.R;
 
 import nath.ariel.sellit_v6.databinding.ActivitySellerBinding;
@@ -11,8 +20,11 @@ import nath.ariel.sellit_v6.databinding.ActivitySellerBinding;
 
 public class SellerActivity extends AppCompatActivity {
 
-    private ActivitySellerBinding binding ;
+    private DatabaseReference database;
 
+
+    private ActivitySellerBinding binding ;
+    private FirebaseAuth firebaseAuth ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +34,30 @@ public class SellerActivity extends AppCompatActivity {
         binding = ActivitySellerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        firebaseAuth = FirebaseAuth.getInstance();
         //Disable Landscape Mode
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        database = FirebaseDatabase.getInstance().getReference();
+
+        //handle click on logout button
+        binding.itemUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Item(String user_id, String name, String description, int price, boolean available, String photoUrl)
+                String name = String.valueOf(binding.productTitle.getText());
+                String descript = String.valueOf(binding.descript.getText());
+                int price = binding.price.getInputType();
+                Item item = new Item(firebaseAuth.getUid(), name,descript, price, true,
+                        "www.stam.co.il");
+
+                //upload to fire base items
+                System.out.println(binding.productTitle.getText()+" "+binding.descript.getText()+" "+binding.price.getText()+" "
+                        +"www.stam.co.il" + firebaseAuth.getUid());
+                System.out.println("database reference: "+database);
+                database.child("Sellit").child("Items").setValue(item);
+            }
+        });
     }
+
 }
